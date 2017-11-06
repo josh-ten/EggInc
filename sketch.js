c14cb1cbe48ba358b923330a1b00be4317c013f1
s3657249 <s3657249@student.rmit.edu.au>
2017-11-06 18:41:24 +1100

    ChickensDespawnInHousing

--- sketch.js
+++ sketch.js
@@ -16,6 +16,15 @@ function setup() {
 
 function draw() {
 	background(200);
+	for (var i = 0; i < housing.length; i++) {
+		for (var j = 0; j < chickens.length; j++) {
+			if (objectCollision(housing[i], chickens[j])) {
+				chickens.splice(j, 1);
+				j--;
+				console.log("ded");
+			}
+		}
+	}
 	clicker.draw();
 	hatchery.draw();
 	for (var i = 0; i < chickens.length; i++) {
@@ -30,6 +39,22 @@ function mousePressed() {
 	if (collision(mouseX, mouseY, clicker)) {
 		clicker.click();
 	}
+}
+
+function objectCollision(obj1, obj2) {
+	//Ensure x and y are aligned with pos
+	if (obj1.hasOwnProperty('pos')) {
+		obj1.x = obj1.pos.x;
+		obj1.y = obj1.pos.y;
+	}
+	if (obj2.hasOwnProperty('pos')) {
+		obj2.x = obj2.pos.x;
+		obj2.y = obj2.pos.y;
+	}
+	return (obj1.x + obj1.width > obj2.x &&
+			obj1.x < obj2.x + obj2.width &&
+			obj1.y + obj1.height > obj2.y &&
+			obj1.y < obj2.y + obj2.height);
 }
 
 function collision(x, y, obj) {
